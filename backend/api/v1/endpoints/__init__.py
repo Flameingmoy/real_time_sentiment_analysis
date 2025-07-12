2de561a5348cd66f2c637eb842ace68579b86234
Flameingmoy <66133065+Flameingmoy@users.noreply.github.com>
2025-07-12 21:53:59 +0530

    Delete backend directory

--- backend/api/v1/endpoints/__init__.py
+++
@@ -1,3 +0,0 @@
-# backend/api/v1/endpoints/__init__.py
-# This makes the 'endpoints' directory a Python package and helps with imports.
-from . import feedback_router
--- backend/db/__init__.py
+++
@@ -1,6 +0,0 @@
-# backend/db/__init__.py
-from .base_class import Base
-from .models import Feedback, SentimentResult, Embedding
-
-# This makes Base.metadata aware of all models imported above
-# when this __init__.py is processed.
